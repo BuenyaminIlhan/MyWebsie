e37e0c41f504bdebd5d6608590310410ee1f3eec
Bünyamin Ilhan <ilhan.buenyamin@gmail.com>
2023-07-26 00:02:03 +0300

    add footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { MainComponent } from './main/main.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,17 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     MainComponent,
     AboutMeComponent,
     SkillsComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,39 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent {
+  @ViewChild('myForm') myForm!: ElementRef
+  @ViewChild('nameField') nameField!: ElementRef
+  @ViewChild('mailField') mailField!: ElementRef
+  @ViewChild('messageField') messageField!: ElementRef
+  @ViewChild('button') button!: ElementRef
+
+
+  async sendMail() {
+    let nameField = this.nameField.nativeElement;
+    let mailField = this.mailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let button = this.button.nativeElement;
+
+    nameField.disabled = true;
+    mailField.disabled = true;
+    messageField.disabled = true;
+    button.disabled = true;
+
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('message', messageField.value);
+    fd.append('email', mailField.value);
+
+    await fetch('https://ilhan-buenyamin.com/send_mail/send_mail.php',
+      {
+        method: 'POST',
+        body: fd
+      }
+    )
+  }
+}
