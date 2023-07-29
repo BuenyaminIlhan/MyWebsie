4a28024c9ab4a23f5ff9182c7ac6db5dce3362be
Bünyamin Ilhan <ilhan.buenyamin@gmail.com>
2023-07-30 01:25:35 +0200

    add drop-down menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  showDiv = false;
 
+  openDropDownmenu() {
+    this.showDiv = true;
+  }
+
+  closeDropDown() {
+    this.showDiv = false;
+  }
 }
