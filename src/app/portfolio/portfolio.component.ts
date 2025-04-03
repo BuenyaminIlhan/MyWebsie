d754323d6faedccdcfd7a9856366d020e808e877
Büny <ilhan.buenyamin@gmail.com>
2025-04-03 18:34:21 +0200

    update portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -16,17 +16,20 @@ export class PortfolioComponent {
 
   onMouseEnter(index: number) {
     this.isHovered[index] = true;
+  
+    const ids = ['arrow-down', 'arrow-down1', 'arrow-down2', 'arrow-down3', 'arrow-down4'];
+  
+    ids.forEach(id => (document.getElementById(id)!.style.transform = 'rotate(0deg)'));
+  
     setTimeout(() => {
-      document.getElementById('arrow-down').style.transform = 'rotate(150deg)';
-      document.getElementById('arrow-down2').style.transform = 'rotate(-150deg)';
-      document.getElementById('arrow-down3').style.transform = 'rotate(-150deg)';
+      ids.forEach((id, i) => 
+        document.getElementById(id)!.style.transform = i % 2 === 0 ? 'rotate(-150deg)' : 'rotate(150deg)'
+      );
     }, 170);
-    document.getElementById('arrow-down').style.transform = 'rotate(0deg)';
-    document.getElementById('arrow-down2').style.transform = 'rotate(0deg)';
-    document.getElementById('arrow-down3').style.transform = 'rotate(0deg)';
   }
-
+  
   onMouseLeave(index: number) {
     this.isHovered[index] = false;
   }
+  
 }
